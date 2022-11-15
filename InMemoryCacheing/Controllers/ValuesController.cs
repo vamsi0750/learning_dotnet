4bf907443fb31a12988448b7337b4ae44fbfbe38
vamsi krishna <vamsi.n@edifybrokers.com>
2022-11-15 22:40:55 +0530

    added in-memory-chacheing

---
+++ InMemoryCacheing/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCacheing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCacheing;
+        public ValuesController(IMemoryCache memoryCacheing)
+        {
+            _memoryCacheing = memoryCacheing;
+        }
+
+        private int[] GetNumbers()
+        {
+            return new int[] { 1, 2, 3, 3, 4, 4 };
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            int[] result;
+            if(!_memoryCacheing.TryGetValue("data", out result)) 
+            {
+                result = GetNumbers();
+                _memoryCacheing.Set("data", result,System.TimeSpan.FromMinutes(2));
+            }
+            return Ok(result);
+        }
+    }
+}
